4f159f8a6aa7bf287aa761c74e3699b0d312a31e
Sagent69 <jonathanadjetey993@gmail.com>
2025-05-20 22:44:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,9 @@ def webhook():
     }})
 
     return 'OK', 200
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default
+    app.run(host="0.0.0.0", port=port)
+
